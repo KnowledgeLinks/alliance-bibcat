750fb2996c4efd9b6490ab309a8cc76016976140
Jeremy Nelson <jermnelson@gmail.com>
2016-09-12 15:01:11 -0600

    added/fixed default namespaces

--- example_instance/config.py
+++ example_instance/config.py
@@ -2,6 +2,9 @@
 # enter a secret key for the flask application instance
 SECRET_KEY = "enter_a_secret_key_here"
 
+# URL used in generating IRIs
+BASE_URL = "http://bibcat.org/"
+
 # Database REST urls
 TRIPLESTORE_URL = "http://localhost:9999/blazegraph/sparql"
 REPOSITORY_URL = "http://localhost:8080/rest"
@@ -13,19 +16,23 @@ DATASET_URLS = {
     "bibframe_vocab_rdf": "http://id.loc.gov/ontologies/bibframe.rdf"
 }
 DEFAULT_RDF_NS = {
-    "kds": "<http://knowledgelinks.io/ns/data-structures/>",
-    "kdr": "<http://knowledgelinks.io/ns/data-resources/>",
+    "kds": "http://knowledgelinks.io/ns/data-structures/",
+    "kdr": "http://knowledgelinks.io/ns/data-resources/",
     "bf": "http://id.loc.gov/ontologies/bibframe/",
-    "skos": "<http://www.w3.org/2004/02/skos/core#>",
+    "skos": "http://www.w3.org/2004/02/skos/core#",
     "loc": "http://id.loc.gov/authorities/",
+    "mods": "http://www.loc.gov/mods/v3",
     "dc": "http://purl.org/dc/terms/",
-    "es": "<http://knowledgelinks.io/ns/elasticsearch/>",
-    "schema": "<http://schema.org/>",
-    "xsd": "<http://www.w3.org/2001/XMLSchema#>",
-    "owl": "<http://www.w3.org/2002/07/owl#>",
-    "void": "<http://rdfs.org/ns/void#>",
-    "dcterm": "<http://purl.org/dc/terms/>",
-    "dc": "<http://purl.org/dc/elements/1.1/>"
+    "es": "http://knowledgelinks.io/ns/elasticsearch/",
+    "schema": "http://schema.org/",
+    "xsd": "http://www.w3.org/2001/XMLSchema#",
+    "owl": "http://www.w3.org/2002/07/owl#",
+    "void": "http://rdfs.org/ns/void#",
+    "dcterm": "http://purl.org/dc/terms/",
+    "dc": "http://purl.org/dc/elements/1.1/",
+    "dbo": "http://dbpedia.org/ontology/",
+    "dbp": "http://dbpedia.org/property/",
+    "dbr": "http://dbpedia.org/resource/"
 }
 # this is the graph name where application definitions are stored
 RDF_DEFINITION_GRAPH = "<http://knowledgelinks.io/ns/application-framework/>"
