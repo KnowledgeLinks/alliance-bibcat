716a19e165a5dc4fda479a67e65f0ca0bf0983ef
Jeremy Nelson <jermnelson@gmail.com>
2016-10-26 21:45:33 -0600

    Siteindex and full sitemaps for entire triplestore Instances now working.

--- simple.py
+++ simple.py
@@ -1,10 +1,25 @@
+__author__ = "Jeremy Nelson"
+
+import datetime
+import json
+import math
+import os
 import requests
-from flask import Flask, jsonify, render_template, request
+import sys
+import time
+from flask import Flask, render_template, request
+from flask import abort, Response
+from flask_cache import Cache
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
 
 LIBRARIES = dict()
+PROJECT_BASE =  os.path.abspath(os.path.dirname(__file__))
+
+cache = Cache(app, config={"CACHE_TYPE": "filesystem",
+                           "CACHE_DIR": os.path.join(PROJECT_BASE, "cache")})
+
 
 def __setup__():
     global LIBRARIES
@@ -15,6 +30,39 @@ def __setup__():
             "latitude": row.get('lat').get('value'),
             "longitude": row.get('long').get('value')
         }
+    while 1:
+        time.sleep(10)
+        try:
+            bindings = __run_query__(TRIPLESTORE_COUNT)
+            count = int(bindings[0].get("count").get("value"))
+            # Check for Empty triplestore, load data and alliance graphs
+            # if empty
+            if count < 1:
+                # Alliance RDF
+                print("...Loading Alliance RDF Graph")
+                with open(os.path.join(PROJECT_BASE,
+                    "custom",
+                    "alliance.ttl")) as fo:
+                    result = requests.post(
+                        app.config.get("TRIPLESTORE_URL"),
+                        data=fo.read(),
+                        headers={"Content-Type": "text/turtle"})
+                # Now loads all TTL files in data directory
+                data_dir = os.path.join(PROJECT_BASE, "data")
+                data_walker = next(os.walk(data_dir))
+                for ttl_filename in data_walker[2]:
+                    print("...Loading {}".format(ttl_filename))
+                    with open(os.path.join(
+                        PROJECT_BASE,
+                        "data",
+                        ttl_filename), "rb+") as fo:
+                        result = requests.post(
+                            app.config.get("TRIPLESTORE_URL"),
+                            data=fo,
+                            headers={"Content-Type": "text/turtle"})
+            break
+        except:
+            pass
 
 def __run_query__(sparql):
     result = requests.post(app.config.get("TRIPLESTORE_URL"),
@@ -87,6 +135,9 @@ def get_types(uuid):
 @app.route("/<uuid>")
 def instance(uuid):
     uri = "http://bibcat.coalliance.org/{}".format(uuid)
+    bindings = __run_query__(GET_CLASS.format(uri))
+    if len(bindings) < 1:
+        abort(404)
     if request.args.get("vocab") == "bibframe":
         return jsonify({"@context": "http://id.loc.gov/ontologies/bibframe/",
             "@id": uri,
@@ -118,17 +169,34 @@ def instance(uuid):
         "version": "0.5.0"
     }
     # add_isbns(uri)
-    return jsonify(output)
+    return Response(json.dumps(output), mimetype="application/ld+json")
     
 
+@app.route("/siteindex.xml")
+@cache.cached(timeout=86400) # Cached for 1 day
+def site_index():
+    """Generates siteindex XML, each sitemap has a maximum of 50k links
+    dynamically generates the necessary number of sitemaps in the 
+    template"""
+    bindings = __run_query__(INSTANCE_COUNT)
+    count = int(bindings[0].get('count').get('value'))
+    shards = math.ceil(count/50000)
+    xml = render_template("siteindex.xml", 
+            count=range(1, shards+1), 
+            last_modified=datetime.datetime.utcnow().isoformat())
+    return Response(xml, mimetype="text/xml")
+
 @app.route("/sitemap<offset>.xml", methods=["GET"])
+@cache.cached(timeout=86400)
 def sitemap(offset=0):
-    # This should be cached
+    offset = (int(offset)*50000) - 50000
+    sparql = INSTANCES.format(offset)
     result = requests.post(app.config.get("TRIPLESTORE_URL"),
-        data={"query": INSTANCES,
+        data={"query": sparql,
               "format": "json"})
     instances = result.json().get('results').get('bindings')
-    return render_template("sitemap_template.xml", instances=instances)
+    xml = render_template("sitemap_template.xml", instances=instances)
+    return Response(xml, mimetype="text/xml")
 
 
 PREFIX = """PREFIX bf: <http://id.loc.gov/ontologies/bibframe/>
@@ -136,7 +204,6 @@ PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX relators: <http://id.loc.gov/vocabulary/relators/>
 PREFIX schema: <http://schema.org/>
 """
-
 
 CREATORS = PREFIX + """
 
@@ -155,7 +222,6 @@ WHERE {{
 }} ORDER BY ?name"""
 
 
-
 DETAIL = PREFIX + """
 
 SELECT DISTINCT ?instance ?date
@@ -165,14 +231,29 @@ WHERE {
     ?process bf:generationDate ?date .
 } LIMIT 100"""
 
+GET_CLASS = PREFIX + """
+
+SELECT DISTINCT ?type_of 
+WHERE {{
+    <{0}> rdf:type ?type_of .
+}}"""
+
 INSTANCES = PREFIX + """
 
 SELECT DISTINCT ?instance ?date
-WHERE {
+WHERE {{
     ?instance rdf:type bf:Instance .
     ?instance bf:generationProcess ?process .
     ?process bf:generationDate ?date .
-} LIMIT 100"""
+}} ORDER BY ?instance
+LIMIT 50000
+OFFSET {0}"""
+
+INSTANCE_COUNT = PREFIX + """
+SELECT (count(*) as ?count) WHERE {
+   ?s rdf:type bf:Instance .
+}"""
+
 
 ITEM = PREFIX + """
 
@@ -180,6 +261,7 @@ SELECT DISTINCT ?item
 WHERE {{
     ?item bf:itemOf <{0}> .
 }}""" 
+
 
 LIBRARY = PREFIX + """
 
@@ -209,7 +291,13 @@ WHERE {{
    OPTIONAL {{ ?title bf:subtitle ?subtitle }}
 }}"""
 
+TRIPLESTORE_COUNT = """SELECT (count(*) as ?count) WHERE {
+   ?s ?p ?o .
+}"""
+
+
+
+__setup__()
 
 if __name__ == '__main__':
-    __setup__()
     app.run(debug=True)
